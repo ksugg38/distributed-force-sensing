c8841e331f9c8c4c368c74ee94ab3dab37a9756b
Katie Sugg <97493239+ksugg38@users.noreply.github.com>
2024-06-20 16:43:03 -0400

    Ironed out coordinate generation bugs.

--- myenv/coordinates.py
+++ myenv/coordinates.py
@@ -10,8 +10,9 @@ class PointPlotter:
         self.master = master
         master.title("Footpath Generator")
 
-        self.figure, self.axes = plt.subplots(figsize=(4, 4))
-        self.style()
+        # Initialize graph
+        self.figure, self.axes = plt.subplots(figsize=(5, 5))
+        self.style_2d()
         self.axes.set_xlim(0, 100)
         self.axes.set_ylim(0, 100)
         self.points = []
@@ -23,7 +24,8 @@ class PointPlotter:
         # in the GUI app
         self.canvas.get_tk_widget().pack()
 
-        self.canvas.mpl_connect('button_press_event', self.onclick)
+        self.click_cid = self.canvas.mpl_connect(
+            'button_press_event', self.onclick)
 
         self.clear_button = tk.Button(master, text="Clear Points",
                                       command=self.clear_points)
@@ -42,26 +44,16 @@ class PointPlotter:
         # "Packs" button into tk window
         self.save_coordinates_button.pack()
 
-    # maybe try to make an unclick, where it will take away a point
-
     def onclick(self, event):
-        x = round(event.xdata, 8)
-        z = round(event.ydata, 8)
+        x = round(event.xdata, 5)
+        z = round(event.ydata, 5)
         y = 0
         # Add point to graph
         if x is not None and z is not None:
-            # You cand append as x, y, z. Then give an option to change the y
-            # self.points.append((x, y, z))
-            # self.axes.plot(x, z, 'ro')
-            # self.canvas.draw()
-
-            # self.entry = tk.Entry(self)
-            # self.entry.pack()
-            # self.entry.delete(0, tk.END)
-            # self.entry.insert(tk.END, "Please enter your number")
             self.show_entry_popup(x, y, z)
 
     def show_entry_popup(self, x, y, z):
+        # Creates confirmation window
         popup = tk.Toplevel(self.master)
         popup.title("Enter Point")
 
@@ -103,49 +95,34 @@ class PointPlotter:
         error_label.pack()
 
     def clear_points(self):
+        # Empty points
         self.points = []
-        self.axes.clear()
-        self.style()
+        self.figure.clear()
+
+        # Redraw 2D graph
+        self.axes = self.figure.add_subplot(111)
+        self.style_2d()
         self.canvas.draw()
+        self.enable_onclick()
 
     def create_footpath(self):
         # Clear existing plot and reset style
-        self.axes.clear()
-        self.style()
+        self.figure.clear()
+        self.axes = self.figure.add_subplot(111, projection='3d')
+        self.style_3d()
 
         # Create bezier curve
         curve_points = self.create_bezier_curve()
 
-        # Plot curve
-        # self.axes.plot(curve_points[:, 0], curve_points[:, 1],
-        #                '-o', label='Bezier Curve')
+        # Plot 3D curve
+        if curve_points is not None:
+            x, y, z = zip(*curve_points)
+            self.axes.plot(x, y, z, 'b-', label='Bezier Curve')
+            self.axes.legend()
+            self.canvas.draw()
 
-        # # Plot control points in red
-        # self.axes.plot(input_array[:, 0], input_array[:, 1],
-        #                'ro-', label='Control Points')
-
-        # self.style()
-        # self.axes.legend()
-
-        # # Redraw canvas
-        # self.canvas.draw()
-
-        # Create a 3D plot
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-
-        # Extract x, y, z coordinates for plotting
-        x, y, z = zip(*curve_points)
-
-        # Plot curve_points on 3D plot
-        ax.plot(x, y, z)
-
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-
-        # Show plot
-        plt.show()
+        # Disable onclick
+        self.disable_onclick()
 
     def save_coordinates(self):
         # Create bezier curve
@@ -154,18 +131,25 @@ class PointPlotter:
         # Save ouput to csv file
         np.savetxt("coordinates2.csv", curve_points,
                    delimiter=",")
-        print("saved coordinates.")
 
-    # Helper style function
-    def style(self):
-        # Style attributes
+    # Helper style functions
+    def style_2d(self):
         self.axes.grid(True)
         self.axes.set_xlabel('X')
         self.axes.set_ylabel('Z')
-        self.axes.set_xlim(0, 10)
-        self.axes.set_ylim(0, 10)
+        self.axes.set_xlim(0, 100)
+        self.axes.set_ylim(0, 100)
 
-    # Helper function
+    def style_3d(self):
+        self.axes.grid(True)
+        self.axes.set_xlabel('X')
+        self.axes.set_ylabel('Y')
+        self.axes.set_zlabel('Z')
+        self.axes.set_xlim(0, 100)
+        self.axes.set_ylim(0, 20)
+        self.axes.set_zlim(0, 100)
+
+    # Creates bezier curve
     def create_bezier_curve(self) -> list:
         input_array = np.array(self.points)
         if len(input_array) < 2:
@@ -181,6 +165,14 @@ class PointPlotter:
         curve_points = bezier_segment(t_values)
         curve_points = np.round(curve_points, 8)
         return curve_points
+
+    # Disables during 3D graph
+    def disable_onclick(self):
+        self.canvas.mpl_disconnect(self.click_cid)
+
+    def enable_onclick(self):
+        self.click_cid = self.canvas.mpl_connect(
+            'button_press_event', self.onclick)
 
 
 def main():
