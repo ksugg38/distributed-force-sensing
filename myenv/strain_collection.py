38bd653abc04d518243c2c8d82d4c0b8517a5b4f
Katie Sugg <97493239+ksugg38@users.noreply.github.com>
2024-06-24 16:07:33 -0400

    Output strain data to csv.

--- myenv/strain_collection.py
+++ myenv/strain_collection.py
@@ -1,62 +1,80 @@
 import serial
 import pandas as pd
+import numpy as np
 
 
 def read_strain(port_path: str, baudrate: int):
-    ser = serial.Serial(port_path, baudrate)  # open serial port
-    # ser.timeout = 1 ?
-    # print(ser.name)         # check which port was really used
+    # Opens serial port
+    ser = serial.Serial(port_path, baudrate)
 
-    # ser.open()
-    # if not ser.is_open():
-    #     raise Exception("Serial port is not open.")
-
+    # Empty buffer and sends byte to start recieving data
     ser.reset_input_buffer()
     ser.write(bytes([1]))
 
+    # Wait for byte to arrive
     while ser.in_waiting == 0:
-        # Wait for a byte to arrive
         pass
 
+    # Initalize enpty strain list
     strain = []
 
+    # Data has arrived
     while ser.in_waiting:
-        # Reads 1 byte and converts into an integer
+        # Initalize temporarly data list
         data = []
+        # Reads 4 bytes and appends to data list
+        # 4 because of how packets are structured and sent
         data.append(int.from_bytes(ser.read(1), 'big'))
         data.append(int.from_bytes(ser.read(1), 'big'))
         data.append(int.from_bytes(ser.read(1), 'big'))
         data.append(int.from_bytes(ser.read(1), 'big'))
 
+        # Checksum matches, save the strain info
         checksum = data[0] + data[1] + data[2]
         if checksum == data[3]:
-            # Checksum matches, save the strain info
             low_byte = data[1]
             high_byte = data[2]
             try:
+                # Adds strain value to strain list
                 strain.append(high_byte * 256 + low_byte)
             except IndexError:
-                print("index error")
                 pass
-    # ser.close()             # close port
-    print(strain)
-    return (strain)
+
+    # Returns strain list
+    return strain
 
 
 def main():
+    # Change depending on number of input pins
+    desired_len = 10
+
+    # Create empty dataframe
+    df = pd.DataFrame(columns=range(desired_len))
+
+    # Change for desired number of data points
+    data_points = 1000
     i = 0
-    df = pd.DataFrame()
-    while i < 1000:
+    while i < data_points:
+
+        # Store strain output as an array
         output = read_strain("/dev/cu.usbmodem14201", 9600)
-        output = pd.DataFrame(output)
 
-        # df.append(output, ignore_index=True)
-        df = pd.concat([df, output])
+        # Deal with cases where output != desired_len
+        if len(output) > desired_len:
+            # Truncates extra numbers
+            output = output[:desired_len]
+        elif (len(output) < desired_len):
+            # Pads with NaN to reach desired_len
+            output = output + [np.nan] * (desired_len - len(output))
 
+        # Adds output as a row to dataframe
+        df.loc[len(df)] = output
         i += 1
 
+    # Ouputs dataframe to csv
     df.to_csv("strain.csv", index=False)
 
 
+# Automatically runs main when you run the file
 if __name__ == "__main__":
     main()
